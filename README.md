d1d7d408e5cffef643cae2b71472492ae53c4369
DylGit3 <senddylmail27@gmail.com>
2025-06-17 14:58:45 -0700

    restructuring

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ParkSafe-LA
+
+🗃️ Extracted Features
+For this project, I extracted a focused subset of columns from the raw parking citation dataset to streamline preprocessing and modeling:
+
+    issue_date – the date the citation was issued
+    issue_time – the time (in HHMM format) the citation was issued
+    make – the vehicle make (e.g., Toyota, Ford)
+    violation_description – a textual description of the violation
+    fine_amount – the amount of the citation in USD
+    loc_long – longitude of where the citation occurred
+    loc_lat – latitude of where the citation occurred
+    violation_code – numeric violation code associated with the citation
+These columns were used for cleaning, feature engineering, ZIP code mapping, and training the ML model to predict parking ticket risk.
--- source/preprocess.py
+++ source/preprocess.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
-df = pd.read_csv("US.txt", sep='\t', header=None)
 
+# US.txt preprocessing
+US_zip_df = pd.read_csv("datasets/US.txt", sep='\t', header=None)
 
-df.columns = [
+US_zip_df.columns = [
     'country', 'zip', 'city', 'state_name', 'state_abbr',
     'county', 'county_code', 'col_8', 'col_9',
     'latitude', 'longitude', 'col_12'
 ]
 
-ca_zips = df[df['state_abbr'] == 'CA']
+ca_zips = US_zip_df[US_zip_df['state_abbr'] == 'CA']
 
 la_zips = ca_zips[ca_zips['county'].str.contains(
     'Los Angeles', case=False, na=False)]
@@ -18,4 +19,9 @@ la_zip_centroids = la_zips[['zip', 'city', 'latitude', 'longitude']]
 
 la_city_zips = la_zip_centroids[la_zip_centroids['city'] == 'Los Angeles']
 
-print(la_city_zips.head(100))
+# zip, lat, long
+la_city_zips_clean = la_city_zips.drop(['city'], axis=1)
+
+print(la_city_zips_clean.head(5))
+
+la_city_zips_clean.to_csv("data/raw/la_zip_centroids.csv", index=False)
